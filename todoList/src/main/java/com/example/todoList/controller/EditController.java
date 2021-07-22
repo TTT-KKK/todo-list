ba13eb4ea44827b9308485ac2bfa2510af1c77f4
TTT-KKK <k5terada.tennis@gmail.com>
2021-07-22 13:21:23 +0900

    create edit

---
+++ todoList/src/main/java/com/example/todoList/controller/EditController.java
@@ -0,0 +1,24 @@
+package com.example.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * editコントローラー
+ * 
+ */
+@RequestMapping("/edit")
+@Controller
+public class EditController {
+	
+	/**
+	 * (S03)タスク編集画面遷移
+	 * 
+	 * @return
+	 * 		edit.html
+	 */
+	@RequestMapping("")
+	public String index() {
+		return "edit"; 
+	}
+}
