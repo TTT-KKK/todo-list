ad17e5bb115409875cc502995f8da7eae9f9d42b
TTT-KKK <k5terada.tennis@gmail.com>
2021-07-22 13:21:05 +0900

    create input

---
+++ todoList/src/main/java/com/example/todoList/controller/InputController.java
@@ -0,0 +1,24 @@
+package com.example.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * inputコントローラー
+ *
+ */
+@RequestMapping("/input")
+@Controller
+public class InputController {
+
+	/**
+	 * (S02)新規登録画面遷移
+	 * 
+	 * @return
+	 * 		input.html
+	 */
+	@RequestMapping("")
+	public String index() {
+		return "input"; 
+	}
+}
