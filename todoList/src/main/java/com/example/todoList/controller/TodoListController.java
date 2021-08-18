00342c55a183fed20e37cf8a7fd5fa11373383e8
TTT-KKK <k5terada.tennis@gmail.com>
2021-08-18 22:23:01 +0900

    Create TodoList screen

---
+++ todoList/src/main/java/com/example/todoList/controller/TodoListController.java
@@ -0,0 +1,24 @@
+package com.example.todoList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ToDoListコントローラー
+ *
+ */
+@RequestMapping("/")
+@Controller
+public class TodoListController {
+
+	/**
+	 * (S01)初期画面遷移
+	 *
+	 * @return
+	 * 		TodoList.html
+	 */
+	@RequestMapping("")
+	public String index() {
+		return "TodoList";
+	}
+}
